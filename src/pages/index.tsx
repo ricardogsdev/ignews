c48ff110eab9b2d34021d356f50f17ea76df51e3
Ricardo Gomes <ricardogsdev@gmail.com>
2022-04-22 17:50:24 -0300

    criando o layout da home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import styles from './home.module.scss';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+          
+          <SubscribeButton />
+        </section>
+
+        <img src='/images/avatar.svg' alt='Girl coding' />
+      </main>
+    </>
+  )
+}
